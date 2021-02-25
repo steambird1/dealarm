cb6ee43b2f8a6c98fbf5c908260c49179ed44719
steambird1 <steambird@163.com>
2021-02-25 10:22:51 +0800

    completed most of things

--- DeAlarm/Form1.Designer.vb
+++ DeAlarm/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form 重写 Dispose，以清理组件列表。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,123 @@ Partial Class Form1
     '注意: 以下过程是 Windows 窗体设计器所必需的
     '可以使用 Windows 窗体设计器修改它。  
     '不要使用代码编辑器修改它。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Pick a time:"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(28, 50)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(230, 21)
+        Me.DateTimePicker1.TabIndex = 1
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time
+        Me.DateTimePicker2.Location = New System.Drawing.Point(273, 50)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(230, 21)
+        Me.DateTimePicker2.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 131)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Time remaining: "
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(28, 158)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(475, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(100, 84)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(158, 31)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(273, 84)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(158, 31)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.BalloonTipText = "Times up. Press [STOP] in main program, or program will alarm after 5 minutes."
+        Me.NotifyIcon1.BalloonTipTitle = "Dealarm notice"
+        Me.NotifyIcon1.Text = "Times up in you setted alarm."
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(524, 193)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Dealarm clock"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer2 As Timer
 End Class
--- DeAlarm/Form1.vb
+++ DeAlarm/Form1.vb
@@ -1,3 +1,68 @@
 ﻿Public Class Form1
+    Dim msec As Long
+    Dim ende As Boolean
+    Dim dbase As Long
+    Dim cd As Date
 
+    Sub alarmup()
+        NotifyIcon1.ShowBalloonTip(5000)
+        MsgBox("Times up! Please stop the timer, otherwise will alarm after 5 min.", vbInformation + vbSystemModal)
+        If Not ende Then
+            msec = 300
+            Timer2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'MsgBox(DateDiff("n", DateTime.Now, CDate(Format(DateTime.Now, "yyyy/MM/dd") & " 13:00")))
+        'NotifyIcon1.ShowBalloonTip(2000)
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+        cd = CDate(Format(DateTimePicker1.Value.Date, "yyyy/MM/dd") & " " & DateTimePicker2.Text)
+        'MsgBox(Timer1.Enabled)
+        ende = False
+        'MsgBox(DateTimePicker2.Text)
+        'MsgBox(Format(DateTimePicker1.Value.Date, "yyyy/MM/dd") & " " & DateTimePicker2.Text)
+        dbase = DateDiff("s", DateTime.Now, cd)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim d As Long
+        d = DateDiff("s", DateTime.Now, cd)
+        'MsgBox(d)
+        If d <= 0 Then
+            Timer1.Enabled = False
+            Label2.Text = "Times up!"
+            ProgressBar1.Value = ProgressBar1.Minimum
+            alarmup()
+        Else
+            Dim m As Long
+            Dim s As Long
+            s = d Mod 60
+            m = Math.Floor(d / 60)
+            Label2.Text = "Time remaining: " & m.ToString("00") & ":" & s.ToString("00")
+            ProgressBar1.Value = Math.Ceiling((d * 100) / dbase)
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        msec = msec - 1
+        If msec = 0 Then
+            Timer2.Enabled = False
+            alarmup()
+        End If
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+        ende = True
+    End Sub
 End Class
